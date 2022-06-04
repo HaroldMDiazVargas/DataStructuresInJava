e0b72767a4f2d090163040ec492edbdc9349e69e
Harold Diaz <harold.diazv1@gmail.com>
2022-06-04 13:41:20 -0500

    Most used growth rates

---
+++ 1. TheBigONotation/6. O(2^n).java
@@ -0,0 +1,15 @@
+// Exponential growth O(2^n)
+
+// The exponential growth is the opposite of Logarithmi growth
+// Logarithmic curve => slow down as the input size grows
+// Exponential curve => grows faster and faster
+// An algorithm that runs in exponential time is not scalable at all => very slow very soon
+
+// These growth rates we have seen are not the only ones => but we see most all the time
+
+//  CONSTANT    O(1)
+//  LOGARITHMIC O(log n)
+//  LINEAR      O(n)
+//  QUADRATIC   O(n^2)
+//  EXPONENTIAL O(2^n)
+ 
